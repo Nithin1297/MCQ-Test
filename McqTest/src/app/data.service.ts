73a717395ef558c82b6d942124cd75affd716c17
Nithin1297 <nithin.madduri@proclink.com>
2024-08-06 17:41:54 +0530

    fetch questions done

---
+++ McqTest/src/app/data.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  constructor() {}
+
+  getAllQuestions() {
+    return fetch('https://66b211521ca8ad33d4f66790.mockapi.io/Questions').then(
+      (res) => res.json()
+    );
+  }
+}
