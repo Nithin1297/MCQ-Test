e6dd7c5efb21f22bdfc44f1949270c396d206e4d
Bhanusri-1320 <bhanusri.gangadevi@proclink.com>
2024-08-07 11:40:03 +0530

    one method added in service

--- McqTest/src/app/data.service.ts
+++ McqTest/src/app/data.service.ts
@@ -8,13 +8,20 @@ export class DataService {
   constructor() {}
 
   getAllQuestions() {
-    return fetch('https://66b211521ca8ad33d4f66790.mockapi.io/Questions').then(
+    return fetch('https://66b211521ca8ad33d4f66790.mockapi.io/Questions')
+      .then((res) => res.json())
+      .then((data) => {
+        this.questions = data;
+      });
+  }
+
+  loadQuestionByIdex(idx: any) {
+    return this.questions[idx];
+  }
+
+  getQuestionById(id: number) {
+    return fetch(`https://backend-project-2s74.onrender.com/form/${id}`).then(
       (res) => res.json()
     );
-  }
-  getQuestionById(id: number) {
-    return fetch(
-      `https://66b211521ca8ad33d4f66790.mockapi.io/Questions/${id}`
-    ).then((res) => res.json());
   }
 }
