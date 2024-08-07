b9aede0d50428077b53ed54fdab20c6321be5d27
Bhanusri-1320 <bhanusri.gangadevi@proclink.com>
2024-08-07 17:33:54 +0530

    working

--- McqTest/src/app/home/home.component.ts
+++ McqTest/src/app/home/home.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { DataService } from '../data.service';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule],
+  imports: [
+    RouterModule,
+    MatButtonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
@@ -18,3 +27,4 @@ export class HomeComponent {
     this.dataService.getAllQuestions();
   }
 }
+import { MatButtonModule } from '@angular/material/button';
